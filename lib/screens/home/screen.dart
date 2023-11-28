11351c9c254bec6385ca250592a5b34b9ef9846a
syehan <muhammad.syehan@dkatalis.com>
2023-11-28 19:48:27 +0700

    feat: create home screen

--- lib/screens/home/screen.dart
+++ lib/screens/home/screen.dart
@@ -10,191 +10,333 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 29, 112, 84),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Good Afternoon,',
-                  style: TextStyle(fontSize: 14),
-                ),
-                Text(
-                  'Alfi Gufron',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
-            ),
-            Container(
-              child: Image.asset(
-                'assets/images/notif.png',
-                width: 40,
-                height: 40,
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Container(
-        margin: EdgeInsets.fromLTRB(22, 387, 22, 0),
+    return BaseLayout.customAppBar(
+      backgroundColor: Colors.white,
+      contentPadding: EdgeInsets.zero,
+      appBarBackgroundColor: const Color(0xff2A7C76),
+      appBar: Container(
+        padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 24),
+        color: const Color(0xff2A7C76),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  'Transaction History',
-                  style: TextStyle(fontSize: 18),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Text(
-                    'See All',
-                    style: TextStyle(fontSize: 17, color: Colors.blue),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: 25),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Indonesia University Payroll',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Text(
-                          'Today',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 16),
                     Text(
-                      '+ Rp 10,000,000',
-                      style: TextStyle(fontSize: 16, color: Colors.green),
+                      'Good Afternoon,',
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    ),
+                    Text(
+                      'Alfi Gufron',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
                   ],
                 ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Starbucks Coffee',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Text(
-                          'Yesterday',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      '- Rp 110,000',
-                      style: TextStyle(fontSize: 16, color: Colors.red),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Uxertiv Payroll',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Text(
-                          'Sep 25, 2023',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 16),
-                    Text(
-                      '+ Rp 2,500,000',
-                      style: TextStyle(fontSize: 16, color: Colors.green),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Youtube Premium',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Text(
-                          'Sep 10, 2023',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 16),
-                    Text(
-                      '- Rp 109,890',
-                      style: TextStyle(fontSize: 16, color: Colors.red),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Spotify Premium',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Text(
-                          'Today',
-                          style: TextStyle(fontSize: 13, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 16),
-                    Text(
-                      'Sep 10, 2023',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ],
+                Image.asset(
+                  'assets/images/notif.png',
+                  width: 40,
+                  height: 40,
                 ),
               ],
             ),
           ],
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: 202,
+            width: 374,
+            padding: const EdgeInsetsDirectional.symmetric(
+                vertical: 25, horizontal: 20),
+            decoration: BoxDecoration(
+                color: const Color(0xff3E7C78),
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Rp 29,843,000',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(height: 33),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 24,
+                              height: 24,
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.15),
+                                  borderRadius: BorderRadius.circular(40)),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.arrow_downward,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 6,
+                            ),
+                            const Text(
+                              'income',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const Text(
+                          'Rp 10,000,000',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 24,
+                              height: 24,
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.15),
+                                  borderRadius: BorderRadius.circular(40)),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.arrow_upward,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 6,
+                            ),
+                            const Text(
+                              'Expenses',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const Text(
+                          'Rp 110,000',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          Container(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Transaction History',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Text(
+                        'See All',
+                        style: TextStyle(fontSize: 17, color: Colors.blue),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 25),
+                const Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Indonesia University Payroll',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Today',
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          '+ Rp 10,000,000',
+                          style: TextStyle(fontSize: 16, color: Colors.green),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Starbucks Coffee',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Yesterday',
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          '- Rp 110,000',
+                          style: TextStyle(fontSize: 16, color: Colors.red),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Uxertiv Payroll',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Sep 25, 2023',
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          '+ Rp 2,500,000',
+                          style: TextStyle(fontSize: 16, color: Colors.green),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Youtube Premium',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Sep 10, 2023',
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          '- Rp 109,890',
+                          style: TextStyle(fontSize: 16, color: Colors.red),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Spotify Premium',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              'Today',
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          'Sep 10, 2023',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
