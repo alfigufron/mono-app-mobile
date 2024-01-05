491384e6aa9ca8cbc3289016ebb164175344d57f
syehan <muhammad.syehan@dkatalis.com>
2024-01-05 21:04:33 +0700

    feat: integrate profile screen with backend

--- lib/screens/profile/screen.dart
+++ lib/screens/profile/screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mono_app/screens/login/bloc/login_bloc.dart';
+import 'package:mono_app/screens/login/login_bloc_implementor.dart';
 import 'package:mono_app/widgets/common/base_layout.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -11,55 +14,63 @@ class ProfileScreen extends StatelessWidget {
       contentPadding: EdgeInsets.zero,
       title: 'My Profile',
       appBarBackgroundColor: const Color(0xff2A7C76),
-      body: Container(
-        padding: EdgeInsets.all(28),
-        margin: EdgeInsets.only(top: 80),
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
+      body: LoginBlocBuilder(builder: (context, bloc, state) {
+        return Container(
+          padding: const EdgeInsets.all(28),
+          margin: const EdgeInsets.only(top: 80),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            children: [
-              ClipOval(
-                child: Image.network(
-                  'https://ajaib.co.id/wp-content/uploads/2020/06/steve-jobs.jpg',
-                  width: 100.0,
-                  height: 100.0,
-                  fit: BoxFit.cover,
+          child: Center(
+            child: Column(
+              children: [
+                ClipOval(
+                  child: Image.network(
+                    'https://ajaib.co.id/wp-content/uploads/2020/06/steve-jobs.jpg',
+                    width: 100.0,
+                    height: 100.0,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              SizedBox(height: 24),
-              Text(
-                'Muhammad Alfi Apriliansyah',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'alfi.apriliansyah@mono.com',
-                style: TextStyle(
-                  color: Color(0xff438883),
+                const SizedBox(height: 24),
+                Text(
+                  state.user?.name ?? '',
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600),
                 ),
-              ),
-              SizedBox(height: 24),
-              Text(
-                'Log out',
-                style: TextStyle(
-                  color: Color(0xffE21818),
+                const SizedBox(height: 8),
+                Text(
+                  state.user?.email ?? '',
+                  style: const TextStyle(
+                    color: Color(0xff438883),
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(height: 24),
+                InkWell(
+                  onTap: () {
+                    bloc.add(Logout());
+                    Modular.to.navigate('/auth/login');
+                  },
+                  child: const Text(
+                    'Log out',
+                    style: TextStyle(
+                      color: Color(0xffE21818),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
